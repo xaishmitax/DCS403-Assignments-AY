d890f7db16a677f469842541c2bb9c367af7acb2
AIshmita Yonzon <aishmitayonzon04@gmail.com>
2026-02-08 14:06:57 +0545

    Feat: week5 assignments added

---
+++ Week5/DAy1/Classwork/Cw1.cpp
@@ -0,0 +1,22 @@
+// This program calculates the factorial of a number using recursion.
+
+#include <iostream>
+using namespace std;
+int factorial (int n) {
+    if (n==0)
+    return 1;
+    else
+    return n*factorial(n-1);
+}
+int main(){
+    int number;
+    cout<<"Enter a positive integer: ";
+    cin>>number;
+    if (number < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << number << " = " << factorial (number) << endl;
+    }
+    return 0;
+    
+}
